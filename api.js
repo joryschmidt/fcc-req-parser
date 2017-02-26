f54055a6684e46e061d08621fa149b07fcd93a37
Jory Schmidt <joryschmidt1988@gmail.com>
2017-02-26 01:08:55 +0000

    add gitignore and finish api

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 module.exports = function(req, res) {
   var ip = req.headers['x-forwarded-for'];
-  var lang = req.headers['accept-language'];
+  var lang = req.headers['accept-language'].match(/^(.+),/)[1];
   var soft = req.headers['user-agent'].match(/\((.+)\)/)[1];
   
   res.send({
